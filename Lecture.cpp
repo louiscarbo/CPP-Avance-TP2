26809e3f2a113247dccd194fc353ec0dd5129b04
louiscarbo <louis@carbo-estaque.fr>
2025-01-24 17:04:18 +0100

    Ajout des fichiers Lecture.h, Lecture.cpp

---
+++ Lecture.cpp
@@ -0,0 +1,57 @@
+/*************************************************************************
+                    Lecture  -  Permet de lire les logs
+                             -------------------
+    début                : 24/01/2025
+*************************************************************************/
+
+//----- Réalisation de la classe Lecture (fichier Lecture.cpp) -----------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Lecture.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type Xxx::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+
+Lecture::Lecture ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Xxx>" << endl;
+#endif
+} //----- Fin de Xxx
+
+
+Lecture::~Lecture ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Xxx>" << endl;
+#endif
+} //----- Fin de ~Xxx
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
---
+++ Lecture.h
@@ -0,0 +1,64 @@
+/*************************************************************************
+                    Lecture  -  Permet de lire les logs
+                             -------------------
+    début                : 24/01/2025
+*************************************************************************/
+
+//---------- Interface de la classe Lecture (fichier Lecture.h) ----------
+#if ! defined ( LECTURE_H )
+#define LECTURE_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe Lecture
+//  Permet de lire les fichiers de Log et retourne une structure
+//  contenant les informations des logs
+//------------------------------------------------------------------------
+
+class Lecture
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+
+    Lecture ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    ~Lecture ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Xxx>
+
+#endif // LECTURE_H
+
