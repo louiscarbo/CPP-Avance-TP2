948206582c4e3ee6eb6d2867bc914c7a5ea8ddab
Xchel Rico Villa <xricovilla@insa-lyon.fr>
2025-01-31 15:14:35 +0100

    oui

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include  <time.h>
 using namespace std;
+
+void test(){
+    LogLine line ={"0.0.0", tm(01/01/2000), "url.html", 20, 42,};
+}
 
 int main(){
 
